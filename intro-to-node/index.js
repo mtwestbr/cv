63916d185237a229bcb7d192dc888fedb51b3e50
mtwestbr <92880980+mtwestbr@users.noreply.github.com>
2022-04-21 20:39:25 -0500

    Add files via upload

---
+++ intro-to-node/index.js
@@ -0,0 +1,7 @@
+//jshint esversion6
+
+//const fs = require('fs');
+//fs.copyFileSync("file1.txt", "file2.txt");
+const superheroes = require('superheroes');
+var mySuperHerName = superheroes.random();
+console.log(mySuperHerName);
